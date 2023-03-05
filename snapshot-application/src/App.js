4a1f8c87683abdc2e029bee3427892104c9f457e
BharathPenkey <penkybharath@gmail.com>
2023-03-05 22:37:23 +0530

    added snapshot files

---
+++ snapshot-application/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Snapshot from './Components/Snapshot/Snapshot';
+
+function App() {
+  return (
+   <div>
+    <Snapshot/>
+   </div>
+  );
+}
+
+export default App;
